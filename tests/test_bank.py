8541819768a86fd7ae3424f15abdec7377dc6bdb
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-06-17 14:06:23 +0700

    Added initial test_bank.py file

---
+++ tests/test_bank.py
@@ -0,0 +1,11 @@
+import unittest
+import bank
+
+
+class Bank(unittest.TestCase):
+    def test_balance(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
