789bf723bce27826dc571b31abaa7b02601f45bf
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-06-20 12:05:47 +0700

    Added invoker.store_commands() unit test

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -13,5 +13,14 @@ class TestReceiver(unittest.TestCase):
         self.assertEqual(bank.Receiver.action(self), None)
 
 
+class TestInvoker(unittest.TestCase):
+    def test_invoker_store_commands(self):
+        invoker = bank.Invoker()
+        invoker.store_command("duck")
+        invoker.store_command(3)
+        invoker.store_command(True)
+        self.assertListEqual(["duck", 3, True], invoker._commands)
+
+
 if __name__ == '__main__':
     unittest.main()
