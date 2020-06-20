4f18a0a306b94b48ef6f1851cd1b69b291d7b671
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-06-20 11:50:53 +0700

    Added unit tests for command.execute() and receiver.action()

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,10 +1,16 @@
 import unittest
+
 import bank
 
 
-class Command(unittest.TestCase):
-    def test_balance(self):
-        self.assertEqual(bank.Command.execute(), False)
+class TestCommand(unittest.TestCase):
+    def test_command_execute(self):
+        self.assertEqual(bank.Command.execute(self), None)
+
+
+class TestReceiver(unittest.TestCase):
+    def test_receiver_action(self):
+        self.assertEqual(bank.Receiver.action(self), None)
 
 
 if __name__ == '__main__':
