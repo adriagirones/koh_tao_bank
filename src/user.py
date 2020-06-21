f091cf5100529e8c60e61b0ddaacc6100bbb9038
stevenhiltrop <stevenhiltrop@gmail.com>
2020-06-21 16:37:54 +0700

    Added TODO's within the files.

--- src/user.py
+++ src/user.py
@@ -1,5 +1,6 @@
 from bank_methods import BankMethods
 
+# TODO: Move this to the bank.py file and delete user.py
 
 class UserBank:
 
@@ -8,12 +9,12 @@ class UserBank:
         self.name = name
 
     def deposit(self, quantity):
-        self.amount = BankMethods.get_deposit(self,self.amount, quantity)
+        self.amount = BankMethods.get_deposit(self, self.amount, quantity)
 
     def balance(self):
-        self.amount = BankMethods.get_balance(self,self.amount)
+        self.amount = BankMethods.get_balance(self, self.amount)
         return self.amount
 
     def withdraw(self, quantity):
-        self.amount,withdraw_done = BankMethods.get_withdraw(self,self.amount, quantity)
+        self.amount, withdraw_done = BankMethods.get_withdraw(self, self.amount, quantity)
         return withdraw_done
