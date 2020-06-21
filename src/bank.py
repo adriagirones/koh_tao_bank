aafd1d92f81f98ae4b7f71617efe8c02263f74c9
Adrià Gironès <girones94@gmail.com>
2020-06-21 17:11:17 +0700

    Moved user class code to bank.py

--- src/bank.py
+++ src/bank.py
@@ -1,4 +1,23 @@
-import user
+from bank_methods import BankMethods
+
+
+class UserBank:
+
+    def __init__(self, name):
+        self.amount = 0
+        self.name = name
+
+    def deposit(self, quantity):
+        self.amount = BankMethods.get_deposit(self, self.amount, quantity)
+
+    def balance(self):
+        self.amount = BankMethods.get_balance(self, self.amount)
+        return self.amount
+
+    def withdraw(self, quantity):
+        self.amount, withdraw_done = BankMethods.get_withdraw(self, self.amount, quantity)
+        return withdraw_done
+
 
 if __name__ == '__main__':
     adria = user.UserBank('adria')
