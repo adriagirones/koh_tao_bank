f8f9813347153a6a0888e6a2991158f4abff86cf
Steven Hiltrop <stevenhiltrop@gmail.com>
2020-06-21 12:36:09 +0700

    Modified initial test_bank.py file to simplify the program

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,24 +2,30 @@ import unittest
 
 import bank
 
+deposit_command = bank.Deposit("50")
+# deposit_invoker = bank.Invoker(deposit_command)
+# deposit_invoker.execute()
+#
+balance_command = bank.Balance()
+
+
+# balance_invoker = bank.Invoker(balance_command)
+# balance_invoker.execute()
+#
+# withdraw_command = bank.Withdraw("150")
+# withdraw_invoker = bank.Invoker(withdraw_command)
+# withdraw_invoker.execute()
+#
+
 
 class TestCommand(unittest.TestCase):
     def test_command_execute(self):
         self.assertEqual(bank.Command.execute(self), None)
 
 
-class TestReceiver(unittest.TestCase):
-    def test_receiver_action(self):
-        self.assertEqual(bank.Receiver.action(self), None)
-
-
 class TestInvoker(unittest.TestCase):
-    def test_invoker_store_commands(self):
-        invoker = bank.Invoker()
-        invoker.store_command("duck")
-        invoker.store_command(3)
-        invoker.store_command(True)
-        self.assertListEqual(["duck", 3, True], invoker._commands)
+    def test_invoker_execute(self):
+        pass
 
 
 if __name__ == '__main__':
