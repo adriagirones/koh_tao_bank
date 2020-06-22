c0b9f4ccc3beb2408cf32584411c13908023f700
Adrià Gironès <girones94@gmail.com>
2020-06-22 17:28:41 +0700

    Created class Command

--- src/bank.py
+++ src/bank.py
@@ -1,26 +1,15 @@
-class UserBank:
+import abc
 
-    def __init__(self, name):
-        self.amount = 0
-        self.name = name
 
-    def deposit(self, quantity):
-        self.amount += quantity
+class Command:
+    """
+    Parent object executing an abstract method
+    """
 
-    def balance(self):
-        return self.amount
-
-    def withdraw(self, quantity):
-        if self.amount - quantity > 0:
-            self.amount -= quantity
+    @abc.abstractmethod
+    def execute(self) -> None:
+        pass
 
 
 if __name__ == '__main__':
-    adria = UserBank('adria')
-    print(adria.balance())
-    adria.deposit(50)
-    print(adria.balance())
-    print(adria.withdraw(45))
-    print(adria.balance())
-    print(adria.withdraw(100))
-    print(adria.balance())
+    pass
